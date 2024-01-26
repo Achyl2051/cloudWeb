79b57f3abda6ab95c951115c0542c98f1036da5b
RASOLOARISON Tahiry Johary <118507545+Achyl2051@users.noreply.github.com>
2024-01-26 14:00:41 +0300

    iny e

--- src/pages/InsertCategorie.js
+++ src/pages/InsertCategorie.js
@@ -19,7 +19,7 @@ function InsertCategorie() {
         e.preventDefault();
         const params = new URLSearchParams();
         params.append("nom", formData.nom);
-        await axios.post("http://localhost:8080/categorie", params, {
+        await axios.post("http://cloudwebservice-production-7ad7.up.railway.app/categorie", params, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
